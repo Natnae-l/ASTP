4539d94d4ef71a77d9ebacafceb956f3a5cbf5a3
Natnael Teklemariam <natnaelmverse1@gmail.com>
2024-02-03 08:49:56 +0300

    controllers initialized

--- controller/appController.js
+++ controller/appController.js
@@ -1,6 +1,17 @@
 import Song from '../module/Song.js'
 
+let getSong = async (req, res, next) => {
+
+}
+let uploadSong = async (req, res, next) => {
+    
+}
+let updateSong = async (req, res, next) => {
+    
+}
+let deleteSong = async (req, res, next) => {
+    
+}
 
 
-
-export {};
+export {getSong, uploadSong, updateSong, deleteSong};
